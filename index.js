8742fad871b51b92d5a5a7b3e6fa8704be80d5c9
Mark Foster <foster154@gmail.com>
2019-06-28 06:38:49 -0600

    add prettier, get region & bucket from env vars, update readme

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-const AWS = require('aws-sdk')
-const s3Zip = require('s3-zip')
+const AWS = require("aws-sdk");
+const s3Zip = require("s3-zip");
 
 /*
-  From https://github.com/orangewise/s3-zip/blob/master/aws_lambda.md
+  Original version: https://github.com/orangewise/s3-zip/blob/master/aws_lambda.md
 */
 
-exports.handler = function (event, context) {
-  console.log('event', event)
-  
-  const region = event.region
-  const bucket = event.bucket
-  const folder = event.folder
-  const files = event.files
-  const zipFileName = event.zipFileName
+exports.handler = function(event, context) {
+  console.log("event", event);
+
+  const region = process.env.S3_REGION;
+  const bucket = process.env.S3_BUCKET;
+  const folder = event.folder;
+  const files = event.files;
+  const zipFileName = event.zipFileName;
 
   // Create body stream
   try {
-
-    const body = s3Zip.archive({ region: region, bucket: bucket}, folder, files)
-    const zipParams = { params: { Bucket: bucket, Key: folder + zipFileName } }
-    const zipFile = new AWS.S3(zipParams)
-    zipFile.upload({ Body: body })
-      .on('httpUploadProgress', function (evt) { console.log(evt) })
-      .send(function (e, r) { 
-        if (e) {
-          const err = 'zipFile.upload error ' + e
-          console.log(err)         
-          context.fail(err)
-        } 
-        console.log(r) 
-        context.succeed(r)
+    const body = s3Zip.archive(
+      { region: region, bucket: bucket },
+      folder,
+      files
+    );
+    const zipParams = { params: { Bucket: bucket, Key: folder + zipFileName } };
+    const zipFile = new AWS.S3(zipParams);
+    zipFile
+      .upload({ Body: body })
+      .on("httpUploadProgress", function(evt) {
+        console.log(evt);
       })
-
+      .send(function(e, r) {
+        if (e) {
+          const err = "zipFile.upload error " + e;
+          console.log(err);
+          context.fail(err);
+        }
+        console.log(r);
+        context.succeed(r);
+      });
   } catch (e) {
-    const err = 'catched error: ' + e
-    console.log(err)    
-    context.fail(err)
+    const err = "catched error: " + e;
+    console.log(err);
+    context.fail(err);
   }
-
-}
+};
--- readme.md
+++ readme.md
@@ -1,15 +1,24 @@
 # s3-zip-lambda
 
 ## Intro
-This is a simple lamda function that, when invoked, will take the passed in s3 bucket, zip the files, and steam down the zip file.
+This is a simple Lambda function that, when invoked, will take the passed in s3 bucket, zip the files, and steam down the zip file.
 
 This code was orginally copied from [](https://github.com/orangewise/s3-zip/blob/master/aws_lambda.md).
 
-## Dev Setup
+## Local Dev Setup
 
 1. clone this repo
 2. run `yarn install`
 
-## Deployment
+## AWS Lamba Setup
 
-(coming soon)
+- create a Lambda function which has access to the needed S3 bucket(s) where the images will be stored.
+- Set up environment variables: 
+  - S3_REGION ("us-east-1" or equivalent)
+  - S3_BUCKET (name of the bucket where images will be stored)
+- 
+
+## Deployment to AWS Lambda
+
+- zip up `index.js` & `node_modules`
+- upload zip file to AWS Lamba function console
